bcb0483aebb7ef48f85b408e4db01fb44a83e259
raandoom <raandoom@users.noreply.github.com>
2016-12-20 03:22:20 +0300

    moved to /bin/dash and volume update on startup

--- bspwm/scripts/volume.sh
+++ bspwm/scripts/volume.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/dash
 
-NOTIFY="$BSPWM_SCRIPTS_PATH/notify.sh"
-ID_PATH="$BSPWM_SCRIPTS_PATH/.id_volume"
+NOTIFY="${BSPWM_SCRIPTS_PATH}/notify.sh"
+ID_PATH="${BSPWM_SCRIPTS_PATH}/.id_volume"
 
-if [ "$1" == "down" ]
+if [ "$1" = "get" ]
+then
+    TEXT=$(amixer get Master | tail -1)
+elif [ "$1" = "down" ]
 then
     TEXT=$(amixer set Master 2%- unmute | tail -1)
-elif [ "$1" == "up" ]
+elif [ "$1" = "up" ]
 then
     TEXT=$(amixer set Master 2%+ unmute | tail -1)
-elif [ "$1" == "mute" ]
+elif [ "$1" = "mute" ]
 then
     TEXT=$(amixer set Master toggle | tail -1)
 else
@@ -17,16 +20,22 @@ else
     exit
 fi
 
-LVL=$(echo $TEXT | cut -d' ' -f5 | sed 's/\(\[\|%\|\]\)// g')
-STATE=$(echo $TEXT | cut -d' ' -f6 | sed 's/\(\[\|\]\)// g')
+LVL=$(echo ${TEXT} | cut -d' ' -f5 | sed 's/\(\[\|%\|\]\)// g')
+STATE=$(echo ${TEXT} | cut -d' ' -f6 | sed 's/\(\[\|\]\)// g')
 
-if [ ! -f "$ID_PATH" ]
+LVL=$(printf "%3s" ${LVL})
+STATE=$(printf "%-3s" ${STATE})
+
+if [ "$1" != "get" ]
 then
-    ID=0
-else
-    ID=$(cat $ID_PATH)
+    if [ ! -f "${ID_PATH}" ]
+    then
+        ID=0
+    else
+        ID=$(cat ${ID_PATH})
+    fi
+    ${NOTIFY} "" "${ID}" "" "VOLUME" "${STATE} ${LVL}" 3000 > ${ID_PATH}
 fi
 
-$NOTIFY "" "$ID" "" "VOLUME" "$STATE $LVL" 3000 > $ID_PATH
 
-echo "V$STATE $LVL" > "$PANEL_FIFO"
+echo "V${STATE} ${LVL}" > "${PANEL_FIFO}"
